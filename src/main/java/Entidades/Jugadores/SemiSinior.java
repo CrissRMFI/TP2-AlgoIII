9dae6dbcf7851b1916337a4ec95d0046f26fa22a
CrissRMFI <croldan@fi.uba.ar>
2023-11-14 12:16:55 -0300

    Implementacion de evolucion del seniority

--- src/main/java/Entidades/Jugadores/SemiSinior.java
+++ src/main/java/Entidades/Jugadores/SemiSinior.java
@@ -5,11 +5,12 @@ import Entidades.Energia.Energia;
 public class SemiSinior implements Seniority{
     @Override
     public void aumentarEnergia(Energia energia) {
-
+        Energia e = new Energia(5);
+        energia.afectarEnergia(e);
     }
 
     @Override
-    public void ascenderSeniority() {
-
+    public Seniority ascenderSeniority() {
+        return new Senior();
     }
 }
