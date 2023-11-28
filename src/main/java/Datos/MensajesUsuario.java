704153f10ecb4bd3c8220f89b26238d8e716ae4e
CrissRMFI <croldan@fi.uba.ar>
2023-11-28 12:57:05 -0300

    Vista-Login, version 1.0

---
+++ src/main/java/Datos/MensajesUsuario.java
@@ -0,0 +1,6 @@
+package Datos;
+
+public class MensajesUsuario {
+    public String IngresoDeJugadores () {return "INGRESO DE JUGADORES";}
+    public String IninciarJuego () {return "INICIAR JUEGO";}
+}
