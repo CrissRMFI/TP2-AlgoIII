ffd80ef288c15507f01474f6fcfa578e7a80a4e2
CrissRMFI <croldan@fi.uba.ar>
2023-11-29 09:56:08 -0300

    MOdleo de vista login 1.1

--- src/main/java/Datos/MensajesUsuario.java
+++ src/main/java/Datos/MensajesUsuario.java
@@ -3,4 +3,7 @@ package Datos;
 public class MensajesUsuario {
     public String IngresoDeJugadores () {return "INGRESO DE JUGADORES";}
     public String IninciarJuego () {return "INICIAR JUEGO";}
+
+    public String CargarMapa () {return "CARGAR MAPA";}
+    public String TituloAplicacion () {return "ALGO ROMA";}
 }
