12d522a25c193f9203b2e67f6e39e6a6b5449e0b
Martín Alejandro Estrada Saavedra <martin996@hotmail.com.ar>
2023-12-14 02:53:19 -0300

    agregacion sonido menu principal

--- src/main/java/edu/fiuba/algo3/App.java
+++ src/main/java/edu/fiuba/algo3/App.java
@@ -1,13 +1,16 @@
 package edu.fiuba.algo3;
 
-import Componentes.VentanaCantidadJugadores;
 import Datos.MensajesUsuario;
 import Entidades.Errores.*;
 import Vista.*;
 import edu.fiuba.algo3.modelo.AppModelo;
 import javafx.application.Application;
-import javafx.scene.control.Alert;
+import javafx.scene.media.MediaPlayer;
 import javafx.stage.Stage;
+import javafx.scene.media.Media;
+import sonido.ReproductorSonido;
+
+import java.io.File;
 
 
 public class App extends Application {
@@ -21,6 +24,9 @@ public class App extends Application {
         this.modelo = new AppModelo();
 
         AppVistaInicio vistaInicio = new AppVistaInicio(this);
+
+        ReproductorSonido reproductorSonido = new ReproductorSonido("src/main/resources/sonidos/inicio.mp3");
+        reproductorSonido.reproducir();
 
         stage.setTitle(MensajesUsuario.TITULO_JUEGO);
         stage.setScene(vistaInicio.obtenerEscena());
--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,5 +1,6 @@
 module edu.fiuba.algo3 {
     requires javafx.controls;
     requires com.fasterxml.jackson.databind;
+    requires javafx.media;
     exports edu.fiuba.algo3;
 }
